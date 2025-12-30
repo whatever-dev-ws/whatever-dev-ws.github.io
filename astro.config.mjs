eb8819a83eed3a9630d7222ea6c2058b6526814f
Michele Maffei <michele.muffin@gmail.com>
2025-12-30 11:43:48 +0100

    fix: add protocol to url to site option in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'whatever-dev-ws.github.io',
+  site: 'https://whatever-dev-ws.github.io',
   vite: {
     plugins: [tailwindcss()],
     resolve: {
